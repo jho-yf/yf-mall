b5f420e72474ddd4a1047c1677d6afec1219ccc9
jho-yf <xu-jihong@qq.com>
2022-01-02 00:10:00 +0800

    feat: yf-mall-ware使用nacos作为注册中心

--- yf-mall-ware/src/main/java/cn/jho/mall/ware/YfMallWareApplication.java
+++ yf-mall-ware/src/main/java/cn/jho/mall/ware/YfMallWareApplication.java
@@ -3,11 +3,13 @@ package cn.jho.mall.ware;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author JHO xu-jihong@qq.com
  * @date 2022-01-01 11:50
  */
+@EnableDiscoveryClient
 @MapperScan("cn.jho.mall.ware.dao")
 @SpringBootApplication
 public class YfMallWareApplication {
