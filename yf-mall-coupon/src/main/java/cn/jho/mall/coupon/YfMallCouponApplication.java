0cf9b05b20ca0dc26ef5e7e05592aa0e4f132cca
jho-yf <xu-jihong@qq.com>
2022-01-02 00:02:54 +0800

    feat: yf-mall-coupon使用nacos作为注册中心

--- yf-mall-coupon/src/main/java/cn/jho/mall/coupon/YfMallCouponApplication.java
+++ yf-mall-coupon/src/main/java/cn/jho/mall/coupon/YfMallCouponApplication.java
@@ -3,11 +3,13 @@ package cn.jho.mall.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author JHO xu-jihong@qq.com
  * @date 2022-01-01 11:11
  */
+@EnableDiscoveryClient
 @MapperScan("cn.jho.mall.coupon.dao")
 @SpringBootApplication
 public class YfMallCouponApplication {
