25c1b9a24e29c78d1c9adea481d975d7c69c2dfc
jho-yf <xu-jihong@qq.com>
2022-03-27 23:48:23 +0800

    fix: 临时关闭权限校验

--- renren-fast-vue/src/utils/index.js
+++ renren-fast-vue/src/utils/index.js
@@ -16,7 +16,9 @@ export function getUUID () {
  * @param {*} key
  */
 export function isAuth (key) {
-  return JSON.parse(sessionStorage.getItem('permissions') || '[]').indexOf(key) !== -1 || false
+  // TODO 临时关闭权限校验
+  // return JSON.parse(sessionStorage.getItem('permissions') || '[]').indexOf(key) !== -1 || false
+  return true;
 }
 
 /**
