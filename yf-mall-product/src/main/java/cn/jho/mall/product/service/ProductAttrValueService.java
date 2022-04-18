f6a8b9430907b1db99cedf084b9f429804e79ea9
jho-yf <xu-jihong@qq.com>
2022-04-18 08:09:19 +0800

    feat: 添加将spu属性批量保存至某个spu下的方法

--- yf-mall-product/src/main/java/cn/jho/mall/product/service/ProductAttrValueService.java
+++ yf-mall-product/src/main/java/cn/jho/mall/product/service/ProductAttrValueService.java
@@ -1,9 +1,11 @@
 package cn.jho.mall.product.service;
 
+import cn.jho.mall.product.vo.spu.BaseAttrs;
 import com.baomidou.mybatisplus.extension.service.IService;
 import cn.jho.common.utils.PageUtils;
 import cn.jho.mall.product.entity.ProductAttrValueEntity;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,6 +17,20 @@ import java.util.Map;
  */
 public interface ProductAttrValueService extends IService<ProductAttrValueEntity> {
 
+    /**
+     * 按参数分页查询spu属性值
+     *
+     * @param params 查询参数
+     * @return 分页工具 - {@link PageUtils}
+     */
     PageUtils queryPage(Map<String, Object> params);
+
+    /**
+     * 将spu属性批量保存至某个spu下
+     *
+     * @param spuId spu id
+     * @param baseAttrs 属性列表
+     */
+    void saveAttrValues(Long spuId, List<BaseAttrs> baseAttrs);
 }
 
--- yf-mall-product/src/main/java/cn/jho/mall/product/service/impl/ProductAttrValueServiceImpl.java
+++ yf-mall-product/src/main/java/cn/jho/mall/product/service/impl/ProductAttrValueServiceImpl.java
@@ -1,20 +1,31 @@
 package cn.jho.mall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
+import cn.jho.common.utils.PageUtils;
+import cn.jho.common.utils.Query;
+import cn.jho.mall.product.dao.ProductAttrValueDao;
+import cn.jho.mall.product.entity.AttrEntity;
+import cn.jho.mall.product.entity.ProductAttrValueEntity;
+import cn.jho.mall.product.service.AttrService;
+import cn.jho.mall.product.service.ProductAttrValueService;
+import cn.jho.mall.product.vo.spu.BaseAttrs;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import cn.jho.common.utils.PageUtils;
-import cn.jho.common.utils.Query;
+import org.springframework.stereotype.Service;
 
-import cn.jho.mall.product.dao.ProductAttrValueDao;
-import cn.jho.mall.product.entity.ProductAttrValueEntity;
-import cn.jho.mall.product.service.ProductAttrValueService;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Service("productAttrValueService")
 public class ProductAttrValueServiceImpl extends ServiceImpl<ProductAttrValueDao, ProductAttrValueEntity> implements ProductAttrValueService {
+
+    private final AttrService attrService;
+
+    public ProductAttrValueServiceImpl(AttrService attrService) {
+        this.attrService = attrService;
+    }
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -24,6 +35,21 @@ public class ProductAttrValueServiceImpl extends ServiceImpl<ProductAttrValueDao
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public void saveAttrValues(Long spuId, List<BaseAttrs> baseAttrs) {
+        List<ProductAttrValueEntity> valueEntities = baseAttrs.stream().map(attr -> {
+            ProductAttrValueEntity entity = new ProductAttrValueEntity();
+            entity.setAttrId(attr.getAttrId());
+            AttrEntity attrEntity = attrService.getById(attr.getAttrId());
+            entity.setAttrName(attrEntity.getAttrName());
+            entity.setAttrValue(attr.getAttrValues());
+            entity.setQuickShow(attr.getShowDesc());
+            entity.setSpuId(spuId);
+            return entity;
+        }).collect(Collectors.toList());
+        this.saveBatch(valueEntities);
     }
 
 }
